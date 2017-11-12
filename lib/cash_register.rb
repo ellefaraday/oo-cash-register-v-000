021a539322a95c45fe35937f66b4f12a1b090f10
Elle Faraday <faraday.elle@gmail.com>
2017-11-12 17:04:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,4 @@
-class CashRegister 
+class CashRegister
 
   attr_accessor :total
 
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(title, price, quantity=1)
     @total += (price * quantity)
   end
-    
+
   def apply_discount
     @total = @total * @employee_discount
   end
