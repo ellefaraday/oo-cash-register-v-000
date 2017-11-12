176b7db83348335da26faf6d865e62682356ac83
Elle Faraday <faraday.elle@gmail.com>
2017-11-12 17:28:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,6 +21,7 @@ class CashRegister
   def apply_discount
     if @discount != nil
       @total = @total * (1 - (@discount/100))
+      binding.pry
       "After the discount, the total comes to $#{@total}."
     else
       "There is no discount to apply."
